d3a79c44f02b3c0f23a1aeebbe9d6addbf6d1633
zendayat <nmapzendaya@gmail.com>
2024-01-21 23:17:35 +0300

    ...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
 import OutsideClickHandler from "react-outside-click-handler";
 import "./Header.css";
+import { Link } from "react-router-dom";
 function Header() {
   const [menuOpened, setMenuOpened] = useState(false)
   
@@ -14,7 +15,9 @@ function Header() {
   return (
     <section className="h-wrapper text-white">
       <div className="h-container flexCenter innerWidth paddings  py-4">
-        <img src="./logo.png" className=" scale-75 md:scale-100" />
+        <Link to='/'>
+          <img src="./logo.png" className=" scale-75 md:scale-100" />
+        </Link>
         <OutsideClickHandler onOutsideClick={()=>setMenuOpened(false)}>
           <div
             className="h-menu top-24 right-0 bg-white md:bg-transparent rounded-xl text-black md:text-white gap-8 p-12 md:p-0 font-medium md:font-normal md:gap-10 flexCenter transition-all duration-300 ease-in z-[99]"
