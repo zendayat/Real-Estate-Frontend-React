f8a6aa83dd46f0a464c1f99a3ce8d9fc2222b4af
zendayat <nmapzendaya@gmail.com>
2024-01-23 23:13:55 +0300

    userProperties hook done

--- src/hooks/useProperties.jsx
+++ src/hooks/useProperties.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import {useQuery} from 'react-query'
 const useProperties = () => {
-    const {data, isError, isLoading, refetch} = useQuery({
-        queryKey: "allProperties"
-    })
+    const {data, isError, isLoading, refetch} = useQuery(
+        "allProperties",
+        getAllProperties,
+        {refetchOnWindowFocus: false}
+    )
+    return{
+      data,
+      isError,
+      isLoading,
+      refetch
+    }
   return {
     data, isError, isLoading, refetch
   }
